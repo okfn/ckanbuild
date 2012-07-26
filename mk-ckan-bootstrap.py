69ba9eddd0d6b5029deee49af5d14de1ba5f9a25
Ian Murray <ian.c.murray@gmail.com>
2012-07-26 18:00:43 +0100

    Attempt to make the  building of the virtualenv a little more consistent.
    By attempting retries, and failing if it doesn't work.

--- mk-ckan-bootstrap.py
+++ mk-ckan-bootstrap.py
@@ -7,24 +7,51 @@ def extend_parser(optparse_parser):
     optparse_parser.add_option('--ckan-location',
             default='git+https://github.com/okfn/ckan.git#egg=ckan',
             help="Target for 'pip install ...' command to install the CKAN package [default: %default]")
+    optparse_parser.add_option('--pip-download-cache',
+            default=os.path.join(os.getcwd(), 'pip-cache'),
+            help="Where pip can store its cached downloads [default: %default]")
 
 def after_install(options, pyenv):
 
     pip = os.path.join(pyenv, 'bin', 'pip')
-    download_cache = os.path.join(pyenv, '..', 'pip_download_cache')
+    download_cache = options.pip_download_cache
 
     if options.ckan_location.startswith('git+'):
         ckan_dir = os.path.join(pyenv, 'src', 'ckan')
     else:
         ckan_dir = options.ckan_location
 
+    def install_deps(dep=None, deps_file=None, retries=3):
+        '''Helper function for making multiple attempts at installing dependencies.
+
+        The cheeseshop can be a bit flakey with a big long list of dependencies,
+        github too can fail sometimes.
+        '''
+
+        if dep is not None:
+            dep_args = ['-e', dep]
+        elif deps_file is not None:
+            dep_args = ['-r', deps_file]
+        else:
+            raise RuntimeError, 'Need a dependency to install!'
+
+        success = False
+        while not success and retries > 0:
+            retcode = subprocess.call([pip, 'install',
+                                       '--download-cache=%s' % download_cache,
+                                       '--use-mirrors'] + dep_args)
+            success = retcode == 0
+            retries -= 1
+        return success
+
     # Install the CKAN source code into the virtual environment.
-    subprocess.call([pip, 'install', '--download-cache=%s' % download_cache, '-e', options.ckan_location])
+    if not install_deps(dep=options.ckan_location):
+        raise RuntimeError, "Couldn't install CKAN! :-("
 
     # Install additional CKAN dependencies into the virtual environment.
     requirements = os.path.join(ckan_dir, 'pip-requirements.txt')
-    subprocess.call([pip, 'install', '--download-cache=%s' % download_cache, '--use-mirrors', '-r',
-            requirements])
+    if not install_deps(deps_file=requirements):
+        raise RuntimeError, "Couldn't install CKAN's dependencies! :-("
 
     # Create a CKAN config file.
     paster = os.path.join(pyenv, 'bin', 'paster')
@@ -38,7 +65,8 @@ def after_install(options, pyenv):
 
     # Install CKAN's test-specific dependencies into the virtual environment.
     pip_requirements_test = os.path.join(ckan_dir, 'pip-requirements-test.txt')
-    subprocess.call([pip, 'install', '--use-mirrors', '--download-cache=%s' % download_cache, '-r',
-            pip_requirements_test])
+    if not install_deps(deps_file=pip_requirements_test):
+        raise RuntimeError, "Couldn't install CKAN's dependencies! :-("
+
 """))
 f = open('ckan-bootstrap.py', 'w').write(output)
