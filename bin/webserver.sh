62137b5386d523b8f2651900c65bd95a637c445d
Ross Jones <rossdjones@gmail.com>
2012-07-30 13:04:58 +0100

    Removing unnecessary vim

--- bin/webserver.sh
+++ bin/webserver.sh
@@ -9,7 +9,6 @@ sudo $APT install apache2
 sudo $APT install libapache2-mod-wsgi
 sudo $APT install nginx
 
-
 # libpq-dev and python-dev are required for psycopg2 but
 # should also install gcc.
 sudo $APT install python-virtualenv
@@ -20,5 +19,3 @@ sudo cp etc/apache2/sites-available/ckan /etc/apache2/sites-available/
 sudo a2ensite ckan
 sudo a2dissite default
 sudo service apache2 reload
-
-vim /etc/apache2/sites-enabled/ckan
