f44f253e8eb857229247e95f60b64c22a8c824da
Ross Jones <rossdjones@gmail.com>
2012-07-30 14:21:46 +0100

    Removed a2ensite calls and added elastic-search config

--- bin/webserver.sh
+++ bin/webserver.sh
@@ -9,13 +9,7 @@ sudo $APT install apache2
 sudo $APT install libapache2-mod-wsgi
 sudo $APT install nginx
 
-# libpq-dev and python-dev are required for psycopg2 but
-# should also install gcc.
+# libpq-dev and python-dev are required for psycopg2 but they'll
+# also install gcc.
 sudo $APT install python-virtualenv
 sudo $APT install libpq-dev python-dev
-
-sudo cp etc/apache2/sites-available/ckan /etc/apache2/sites-available/
-
-sudo a2ensite ckan
-sudo a2dissite default
-sudo service apache2 reload
