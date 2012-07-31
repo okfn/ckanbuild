660a65a7ecd87e98cdc7a2b876051464372e03ec
Ian Murray <ian.c.murray@gmail.com>
2012-07-31 11:46:46 +0100

    Installation is now a function

--- bin/dbserver.sh
+++ bin/dbserver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # only run with no arguments
-if [ $# -eq 0 ] ; then
-
+init() {
+    
     APT="apt-get -y --quiet"
-
+    
     sudo $APT install htop
     sudo $APT install postgresql-9.1
     sudo $APT install postgresql-9.1-postgis
@@ -13,30 +13,30 @@ if [ $# -eq 0 ] ; then
     sudo $APT install openjdk-6-jdk
     sudo $APT install ufw
     sudo $APT install pgtune
-
+    
     sudo sed -i 's/#JAVA_HOME=/JAVA_HOME=\/usr\/lib\/jvm\/java-1.6.0-openjdk-amd64\//g' /etc/default/jetty
     sudo sed -i 's/#JETTY_PORT=8080/JETTY_PORT=8983/g' /etc/default/jetty
     sudo sed -i 's/#JETTY_HOST.*/JETTY_HOST=127.0.0.1/g' /etc/default/jetty
     sudo sed -i 's/NO_START=1/NO_START=0/g' /etc/default/jetty
-
+    
     sudo sed -i 's/^#kernel\.shmmax =.*/kernel.shmmax = 1143210240/' /etc/sysctl.d/30-postgresql-shm.conf
-
+    
     sudo sysctl -p /etc/sysctl.d/30-postgresql-shm.conf
-
+    
     sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
-
+    
     sudo cp /etc/postgresql/9.1/main/postgresql.conf  /etc/postgresql/9.1/main/postgresql.conf.bak
     sudo pgtune -i /etc/postgresql/9.1/main/postgresql.conf -o /etc/postgresql/9.1/main/postgresql.conf
-
+    
     sudo sed -i "s/^#listen_addresses =.*/listen_addresses = '*'/" /etc/postgresql/9.1/main/postgresql.conf
-
+    
     wget https://raw.github.com/okfn/ckan/master/ckan/config/solr/schema-1.4.xml
-
+    
     sudo cp schema-1.4.xml /etc/solr/conf/schema.xml
     rm schema-1.4.xml
-
+    
     sudo service jetty restart
-
+    
     if sudo grep -q "samenet md5" /etc/postgresql/9.1/main/pg_hba.conf
     then
        echo "samenet was found in /etc/postgresql/9.1/main/pg_hba.conf"
@@ -44,14 +44,14 @@ if [ $# -eq 0 ] ; then
        echo "host all all samenet md5" | sudo tee -a /etc/postgresql/9.1/main/pg_hba.conf
     fi
     
-    exit 0
-
     echo 'Setting up default firewall configuration...'
     sudo ufw default deny
     sudo ufw allow openssh
     sudo ufw enable
     echo 'Done setting up default firewall configuration.'
-fi
+
+    return 0
+}
 
 
 randpass() {
@@ -91,7 +91,7 @@ createdb() {
     sudo -u postgres createdb -O $DB_NAME $DB_NAME
     sudo -u postgres createuser -DRSP $DB_NAME
 
-    # su - postgres -c "psql -c \"ALTER USER \\\"${INSTANCE}\\\" WITH PASSWORD '${password}'\""
+    return 0
 }
 
 PG_PORT="5432"
@@ -109,6 +109,8 @@ addweb() {
 
    sudo ufw allow in on eth1 proto tcp from "$IP_ADDRESS" to any port "$PG_PORT"
    sudo ufw allow in on eth1 proto tcp from "$IP_ADDRESS" to any port "$SOLR_PORT"
+
+   return 0
 }
 
 removeweb() {
@@ -123,9 +125,12 @@ removeweb() {
 
    sudo ufw delete allow in on eth1 proto tcp from "$IP_ADDRESS" to any port "$PG_PORT"
    sudo ufw delete allow in on eth1 proto tcp from "$IP_ADDRESS" to any port "$SOLR_PORT"
+
+   return 0
 }
 
 case "$1" in
+    init) init ;;
     createdb) createdb $2 ;;
     dropdb) dropdb ;;
     listdb) listdb ;;
@@ -135,6 +140,7 @@ case "$1" in
     *) echo "
 
 Commands are:
+init # Install and configure database services: postgres and solr
 createdb 'dbname' # createdb and user
 drobdb 'dbname' # drop db and user
 addweb ipaddress # allow firewall to connect to webserver
