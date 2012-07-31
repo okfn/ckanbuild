8369c9fa262f1789d97220c5cdef9a993ae60217
Ian Murray <ian.c.murray@gmail.com>
2012-07-31 12:16:36 +0100

    Put webserver installation into a bash function

--- bin/webserver.sh
+++ bin/webserver.sh
@@ -1,33 +1,54 @@
-#!/bin/sh
+#!/bin/bash
 
-while true; do
-    read -p "Have you written down the IPs for solr, database and elastic search [y/n]? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-echo ""
-read -p "Please enter the IP for the SOLR server? " SOLR
-read -p "Please enter the IP for the DATABASE server? " DATABASE
-read -p "Please enter the IP for the ELASTIC server? " ELASTIC
-
-echo "Configuring network with: "
-echo " SOLR -> $SOLR"
-echo " DATABASE -> $DATABASE"
-echo " ELASTIC -> $ELASTIC"
-echo ""
-
-while true; do
-    read -p "Should we run with those values [y/n]? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+init() {
+    while true; do
+        read -p "Have you written down the IPs for solr, database and elastic search [y/n]? " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+    
+    echo ""
+    read -p "Please enter the IP for the SOLR server? " SOLR
+    read -p "Please enter the IP for the DATABASE server? " DATABASE
+    read -p "Please enter the IP for the ELASTIC server? " ELASTIC
+    
+    echo "Configuring network with: "
+    echo " SOLR -> $SOLR"
+    echo " DATABASE -> $DATABASE"
+    echo " ELASTIC -> $ELASTIC"
+    echo ""
+    
+    while true; do
+        read -p "Should we run with those values [y/n]? " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+    
+    check_hosts 'solrserver' $SOLR
+    check_hosts 'dbserver' $DATABASE
+    check_hosts 'elasticserver' $ELASTIC
+    
+    APT="apt-get -y --quiet"
+    
+    sudo $APT update
+    sudo $APT upgrade
+    
+    sudo $APT install htop
+    sudo $APT install apache2
+    sudo $APT install libapache2-mod-wsgi
+    sudo $APT install nginx
+    
+    # libpq-dev and python-dev are required for psycopg2 but they'll
+    # also install gcc.
+    sudo $APT install python-virtualenv
+    sudo $APT install libpq-dev python-dev
+}
 
 check_hosts() {
     if grep -q "$1" /etc/hosts
@@ -37,22 +58,13 @@ check_hosts() {
         echo "$2 $1" | sudo tee -a /etc/hosts
     fi
 }
+    
+case ${1-} in
+    init) init ;;
+    *) echo "
 
-check_hosts 'solrserver' $SOLR
-check_hosts 'dbserver' $DATABASE
-check_hosts 'elasticserver' $ELASTIC
+Commands are:
+init # Install and configures apache and nginx
+"
 
-APT="apt-get -y --quiet"
-
-sudo $APT update
-sudo $APT upgrade
-
-sudo $APT install htop
-sudo $APT install apache2
-sudo $APT install libapache2-mod-wsgi
-sudo $APT install nginx
-
-# libpq-dev and python-dev are required for psycopg2 but they'll
-# also install gcc.
-sudo $APT install python-virtualenv
-sudo $APT install libpq-dev python-dev
+esac
