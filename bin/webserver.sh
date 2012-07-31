5575b8e7e005ed4c69b86094485bfdb9b3be1e77
Ian Murray <ian.c.murray@gmail.com>
2012-07-31 12:18:17 +0100

    No need to install python deps...
    as they are installed as part of the CKAN package installation.

--- bin/webserver.sh
+++ bin/webserver.sh
@@ -43,11 +43,6 @@ init() {
     sudo $APT install apache2
     sudo $APT install libapache2-mod-wsgi
     sudo $APT install nginx
-    
-    # libpq-dev and python-dev are required for psycopg2 but they'll
-    # also install gcc.
-    sudo $APT install python-virtualenv
-    sudo $APT install libpq-dev python-dev
 }
 
 check_hosts() {
